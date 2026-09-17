0c541fda7acbf27fd8dc47d31a1528e7349c0c93
unknown <nahuelgauna20@gmail.com>
2026-09-17 11:52:04 -0300

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TiendaLaku/',  // <-- esta línea agregala
   plugins: [react()],
-  base: '/TiendaLaku/',
+  base: '/',
 })
