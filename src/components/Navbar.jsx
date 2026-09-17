826ea9f517dc8e349ba9303dcefb11e271dc3f24
NahuelGauna <125195378+Nahuel-Gauna@users.noreply.github.com>
2026-09-17 10:52:34 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Styles/Navbar.css'; // Ruta corregida
+import '../styles/Navbar.css'; // Ruta corregida
 import logo from '../assets/Logo.png'
 
 const Navbar = () => {
